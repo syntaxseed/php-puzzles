909d3534b42dc8cbc51b0619badb5fe9ab901cf9
Syntaxseed <825423+syntaxseed@users.noreply.github.com>
2020-10-30 10:56:06 -0400

    Modify solution for December. Clean up class.

--- Puzzle_8_Dec_2020_Grid_Mapping/maze.php
+++ Puzzle_8_Dec_2020_Grid_Mapping/maze.php
@@ -2,7 +2,7 @@
 /**
  * Maze Class - Accumulated solutions from current and previous Grid and Maze related puzzles.
  * Each month we will add more to this class.
- * Version 1.8
+ * Version 1.8 - Dec 2020
  * Provides grid-based maze generation and traversal functions. For command-line use.
  * Author: Sherri Wheeler (Twitter @SyntaxSeed) for PHP Architect Magazine.
  */
@@ -15,37 +15,55 @@ class Maze{
     const LIGHT = '░';
     const DARK = '▓';
 
-    public function _construct(){
-        //
+    /**
+     * Initialize the grid with a given width and height and an intial fill value.
+     * Pass zero for height and width to get a random size.
+     */
+    public function __construct(int $width=0, int $height=0, string $initialFill=self::DARK){
+        $this->width = ($width == 0) ? random_int(10, 50) : $width;
+        $this->height = ($height == 0) ? random_int(10, 50) : $height;
+        $this->fillUniform($initialFill);
     }
 
+    /**
+     * Fill the grid with a single initial value.
+     */
+    public function fillUniform(string $fill=self::DARK) : void {
+        $this->grid = array_fill(0, $this->height, array_fill(0, $this->width, $fill));
+    }
+
+    /**
+     * Fill the grid with a supplied 2-dimensional array.
+     */
     public function fillFromArray(array $gridContents) : void {
         $this->width = count($gridContents[0]);
         $this->height = count($gridContents);
         $this->grid = $gridContents;
     }
 
-    public function fillRandom(int $width=0, int $height=0) : void {
-        $this->width = ($width == 0) ? random_int(10, 50) : $width;
-        $this->height = ($height == 0) ? random_int(10, 50) : $height;
-
+    /**
+     * Fill the grid with random dark or light shade.
+     */
+    public function fillRandom() : void {
         for ($i = 0; $i < $this->height; $i++) {
             for ($j = 0; $j < $this->width; $j++) {
                 $this->grid[$i][$j] = random_int(0, 1);
             }
         }
-
     }
 
-    // PHP Puzzles 8 - Dec 2020.
+    /**
+     * PHP Puzzles 8 - Dec 2020.
+     * Return the contents of the grid for display on the command line.
+     */
     public function displayGrid() : string {
         $joined = implode("\n", array_map('implode', $this->grid));
         $joined = str_replace(0, self::LIGHT, $joined);
         $joined = str_replace(1, self::DARK, $joined);
         return $joined;
     }
-
 }
+
 /*
 $maze = new Maze();
 $maze->fillRandom();
